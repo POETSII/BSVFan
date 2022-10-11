84df7f8376fe86023d7c3c3ca7d621b3f2da8609
Franz Fuchs <franz.fuchs@cl.cam.ac.uk>
2022-10-11 13:48:01 +0100

    changed file path to mkDE10FanControl.v

--- de10_fan_hw.tcl
+++ de10_fan_hw.tcl
@@ -40,7 +40,7 @@ add_fileset QUARTUS_SYNTH QUARTUS_SYNTH "" ""
 set_fileset_property QUARTUS_SYNTH TOP_LEVEL mkDE10FanControl
 set_fileset_property QUARTUS_SYNTH ENABLE_RELATIVE_INCLUDE_PATHS false
 set_fileset_property QUARTUS_SYNTH ENABLE_FILE_OVERWRITE_MODE false
-add_fileset_file mkDE10FanControl.v VERILOG PATH ../mkDE10FanControl.v TOP_LEVEL_FILE
+add_fileset_file mkDE10FanControl.v VERILOG PATH mkDE10FanControl.v TOP_LEVEL_FILE
 
 
 # 
